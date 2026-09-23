cacf43ee88b11fa6caff8152e34c4ed71c962035
CarlosJX7 <carlos.jx7@gmail.com>
2026-09-23 15:11:31 +0200

    Update flake8 & mypy

--- parsing.py
+++ parsing.py
@@ -1,41 +1,26 @@
-"""
-# Default A-Maze-ing configuration
-WIDTH=13
-HEIGHT=9
-ENTRY=0,0
-EXIT=12,8
-OUTPUT_FILE=maze.txt
-PERFECT=True
-ALGORITHM=dfs
-SHOW_PATH=False
-THEME=Hedge #optional
-ANIMATE=False #optional
-
-"""
-
 from enum import Enum
-from pydantic import BaseModel, model_validator, Field
+from pydantic import BaseModel, Field, model_validator, ValidationError
+from typing import Annotated
+
+
+PositiveInt = Annotated[int, Field(gt=0)]
+NegativeInt = Annotated[int, Field(gt=0)]
+
 
 class ValidParams(str, Enum):
     WIDTH = "width"
     HEIGHT = "height"
     ENTRY = "entry"
-    EXIT = "exit"
+    EXIT = "exit_p"
     OUTPUT_FILE = "output_file"
     PERFECT = "perfect"
 
-import sys
-from pydantic import BaseModel, Field, model_validator, ValidationError
-from typing import Annotated
-
-PositiveInt = Annotated[int, Field(gt=0)]
-NegativeInt = Annotated[int, Field(gt=0)]
 
 class Config(BaseModel):
     width: int = Field(..., gt=0)
-    height: PositiveInt
+    height: PositiveInt = Field(...)
     entry: tuple[int, int] = Field(...)
-    exit: tuple[int, int] = Field(...)
+    exit_p: tuple[int, int] = Field(...)
     output_file: str = Field(...)
     perfect: bool = Field(...)
     # Optionals
@@ -45,15 +30,20 @@ class Config(BaseModel):
     animate: bool | None = None
 
     @model_validator(mode="after")
-    def validate_args(self):
+    def validate_args(self) -> "Config":
         entry_x, entry_y = self.entry
         if entry_x >= self.width or entry_y >= self.height:
-            raise ValueError("Entry points outside the limits")
+            raise ValidationError("Entry points outside the limits")
+        exit_x, exit_y = self.exit_p
+        if exit_x >= self.width or exit_y >= self.height:
+            raise ValidationError("Exit points outside the limits")
+        if self.exit_p == self.entry:
+            raise ValidationError("Exit point can't be the same as the entry")
         return self
 
     @staticmethod
-    def open_file(path: str) -> dict[str, str]:
-        new_dict = {}
+    def open_file(path: str) -> dict[str, str | bool]:
+        new_dict: dict[str, str | bool] = {}
         line_counter = 0
         with open(path, "r", encoding="utf-8") as file:
             params = {}
@@ -64,7 +54,10 @@ class Config(BaseModel):
                     continue
                 p_key, sep, value = line.partition("=")
                 if not sep:
-                    raise ValueError(f"Error in sign input. Line {line_counter}")
+                    raise ValueError(
+                        f"Error in sign input. Line {line_counter}"
+                        )
+                # validar que exista la clave
                 try:
                     key = ValidParams[p_key]
                 except ValueError:
@@ -72,22 +65,23 @@ class Config(BaseModel):
                 params[key] = value
             for key, value in params.items():
                 match key:
-                    case "entry" | "exit":
-                        value = value.split(",")
-                        value = (value[0], value[1])
+                    case "entry" | "exit_p":
+                        value_splited = value.split(",")
+                        value_tuple = (value_splited[0], value_splited[1])
                 match value:
                     case "False":
-                        value = False
+                        new_dict[key] = False
+                        continue
                     case "True":
-                        value = True
+                        new_dict[key] = True
+                        continue
                 new_dict[key] = value
         return new_dict
 
     @staticmethod
-    def parse_input(input: dict[str, str]):
+    def parse_input(input: dict[str, str | bool]) -> "Config":
         try:
             new = Config.model_validate(input)
-        except ValueError as e:
-            print(e)
-            return
-        print(new)
+        except:
+            raise ValidationError()
+        return new
